2e8f5f535e519502f0e12bef57ccb11cc34abf60
DazaY <139747681+DazaY0@users.noreply.github.com>
2026-06-09 12:23:08 +0200

    Initial Commit

---
+++ HeatMap/HeatMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeatMapApp.swift
+//  HeatMap
+//
+//  Created by Jonas Hafner on 09.06.26.
+//
+
+import SwiftUI
+
+@main
+struct HeatMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
